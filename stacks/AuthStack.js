4bd111229716afbe4c63e1bf614b6df62fbf72d7
Benjamin Seth Becerra <benjaminsethbecerra@Benjamins-MBP.hsd1.ut.comcast.net>
2023-05-19 12:14:36 -0600

    update create delete get list and update.js

--- stacks/AuthStack.js
+++ stacks/AuthStack.js
@@ -1,6 +1,6 @@
 import * as iam from "aws-cdk-lib/aws-iam";
 import { Cognito, use } from "sst/constructs";
-import { StorageStack } from "./StorageStack";
+import { StorageStack } from "./StorageStacks";
 import { ApiStack } from "./ApiStack";
 
 export function AuthStack({ stack, app }) {
