06fb72d5ebcc3854065f02b2601c13d5bde002d8
Benjamin Seth Becerra <benjaminsethbecerra@Benjamins-MBP.hsd1.ut.comcast.net>
2023-05-22 12:46:20 -0600

    added lots, need to do more to make the app better yes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -5,6 +5,8 @@ import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
 
 export default function Links() {
   return (
@@ -13,6 +15,9 @@ export default function Links() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/notes/new" element={<NewNote />} />
+      <Route path="/notes/:id" element={<Notes />} />
+      <Route path="/settings" element={<Settings />} />
+      
         {
         /* Finally, catch all unmatched routes */
         }
